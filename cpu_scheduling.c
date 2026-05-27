2a9219914a7e8bb9b64f530a9f2691bdb9d04e54
ekta242 <ektaeben@gmail.com>
2026-05-27 10:57:36 +0530

    Learned SRTF -Shortest Remaining Time First

--- cpu_scheduling.c
+++ cpu_scheduling.c
@@ -13,7 +13,7 @@ typedef struct process{
     int completion;
 } Process;
  void copyProcess(Process dest[], Process src[], int n) {
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
         dest[i] = src[i];
     }
 float fcfs(Process p[], int n){
@@ -36,4 +36,36 @@ for(int i=0;i<n;i++){
         total_wait+=p[i].waiting;
     }
     return total_wait/n;
+}
+float srtf(Process p[],int n){
+    int complete=0,time=0,min_remain;
+    int shortest=0,found=0;
+    float total_time=0;
+    while (complete!=n){
+        min_remain=INT_MAX;
+        found=0
+        for (int i=0;i<n;i++){
+            if (p[i].arrival<=time &&p[i].remaining>0 && p[i].remaing<min_remain){
+                min_remain=p[i].remaining;
+                shortest=i;
+                found=1;
+            }
+        }
+        if(!found){
+            time++;
+            continue;
+        }
+        p[shortest].remaining--;
+        time++;
+        if (p[shortest].remaining==0){
+            complete++;
+            p[shortest].completion=time;
+            p[shortest].waiting=p[shortest].completion-p[shortest].arrival-p[shortest].burst
+            if(p[shortest].waiting<0)
+                p[shortest].waiting=0;
+            total_time+=p[shortest].waiting;
+        }
+        }
+    return total_time/n;
+    }
 }
